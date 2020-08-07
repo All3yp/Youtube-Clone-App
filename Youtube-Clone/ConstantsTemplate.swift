4c410593c5b2cd266bab748965f9a0f5a4f03e56
All3yp <noyalle@ualberta.ca>
2020-08-07 15:23:07 -0300

    Added ConstantsTemplate

---
+++ Youtube-Clone/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  Youtube-Clone
+//
+//  Created by Alley Pereira on 07/08/20.
+//  Copyright © 2020 Alley Pereira. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+ 
+ Add your own keys bellow and uncomment the code
+ 
+ 
+ struct Constants {
+ static var API_KEY = ""
+ static var PLAYLIST_ID = ""
+ static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+ }
+ */
